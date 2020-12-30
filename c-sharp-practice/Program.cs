d9ea5435280eded891c028eab0eb28e21882b9d9
endlesstrax <ricky@whitelionmedia.com>
2020-12-30 12:41:57 -0500

    Game code added

--- c-sharp-practice/Program.cs
+++ c-sharp-practice/Program.cs
@@ -6,7 +6,37 @@ namespace guess_the_number
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool gameover = false;
+
+            Random rand = new Random();
+            int randomNumber = rand.Next(0, 10);
+            Console.WriteLine(randomNumber);
+
+
+            Console.WriteLine("Guess the Number Game");
+            Console.WriteLine("=====================");
+            Console.WriteLine("");
+            Console.WriteLine("Guess the number from 0 - 10: ");
+
+            while (!gameover)
+            {
+                string guess = Console.ReadLine();
+                int convertedGuess = Convert.ToInt32(guess);
+
+                if (convertedGuess == randomNumber)
+                {
+                    Console.WriteLine("You guessed correctly!");
+                    gameover = true;
+                }
+                else if (convertedGuess > randomNumber)
+                {
+                    Console.WriteLine("Your guess is too high. Try again");
+                }
+                else if (convertedGuess < randomNumber)
+                {
+                    Console.WriteLine("Your guess is too low. Try again");
+                }
+            }
         }
     }
 }
