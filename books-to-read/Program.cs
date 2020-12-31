687d457992ad7b5231bdcc8fcd6a7324a6493fad
endlesstrax <ricky@whitelionmedia.com>
2020-12-31 14:53:38 -0500

    Added new project

---
+++ books-to-read/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace books_to_read
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
