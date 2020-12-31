9e113d08c6e92302f37f46d45b15c9b29604a5d1
endlesstrax <ricky@whitelionmedia.com>
2020-12-31 14:53:48 -0500

    Added new project

---
+++ currency-converter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace currency_converter
+{
+    class Program
+    {
+        // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            string exchangeEndpoint = "https://api.exchangeratesapi.io/latest?base=USD";
+
+            try
+            {
+                string responseBody = await client.GetStringAsync(exchangeEndpoint);
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
+            
+
+
+
+
+        }
+    }
+}
